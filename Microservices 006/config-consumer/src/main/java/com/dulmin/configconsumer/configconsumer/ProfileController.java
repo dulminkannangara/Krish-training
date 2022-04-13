3854b969724e56b5072d87e2ecaccc530adfa229
Dulmin <kkddulmin@gmail.com>
2022-04-13 19:35:25 +0530

    Microservices 006

---
+++ Microservices 006/config-consumer/src/main/java/com/dulmin/configconsumer/configconsumer/ProfileController.java
@@ -0,0 +1,30 @@
+package com.dulmin.configconsumer.configconsumer;
+
+import com.dulmin.configconsumer.configconsumer.model.MemberProfileConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class ProfileController {
+
+    @Autowired
+    MemberProfileConfiguration memberProfileConfiguration;
+
+//    @RequestMapping("/profile")
+//    public MemberProfileConfiguration getConfig(){
+//        return  memberProfileConfiguration;
+//    }
+
+    @RequestMapping("/profile")
+    public String getConfig(Model model){
+        model.addAttribute("model",memberProfileConfiguration.getDefaultModel());
+        model.addAttribute("min",memberProfileConfiguration.getMinRentPeriod());
+
+        return "mprofile";
+    }
+
+}
