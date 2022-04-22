dccc6f32fb57cd071393118a7d7d601c7759af7a
Dulmin <kkddulmin@gmail.com>
2022-04-22 17:32:06 +0530

    Microservices 018

---
+++ Microservices 018/rent-service-A/src/main/java/lk/codelabs/rentcloud/rentservice/RentServiceApplication.java
@@ -0,0 +1,21 @@
+package lk.codelabs.rentcloud.rentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EntityScan(basePackages = "lk.codelabs.rentcloud.model.rent")
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class RentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentServiceApplication.class, args);
+    }
+
+}
